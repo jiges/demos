eb078ea673ea0c7826f8c4b611b9e366b66ead72
jiges <ccr12312@163.com>
2018-12-05 09:17:20 +0800

    加入登录页面

---
+++ pac4j-shiro-demo/src/main/java/com/ccr/pac4jshirodemo/IndexController.java
@@ -0,0 +1,21 @@
+package com.ccr.pac4jshirodemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ccr12312@163.com at 2018-12-5
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
