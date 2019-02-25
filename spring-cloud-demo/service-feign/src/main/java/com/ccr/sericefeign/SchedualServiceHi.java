9ea8c03327d3b57d725904837450294bc118af19
jiges <ccr12312@163.com>
2019-02-25 15:18:02 +0800

    SpringCloud 服务消费者，Feign实现

---
+++ spring-cloud-demo/service-feign/src/main/java/com/ccr/sericefeign/SchedualServiceHi.java
@@ -0,0 +1,17 @@
+package com.ccr.sericefeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 通过@FeignClient（“服务名”），来指定调用哪个服务
+ * @author ccr12312@163.com at 2019-2-25
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
