50f57ec643c098a51a7723a232d5ac371abed0d0
jiges <ccr12312@163.com>
2019-02-25 15:43:58 +0800

    SpringCloud RestTemplate+Ribbon方式实现服务消费的情况下，加入断路器的支持

--- spring-cloud-demo/eureka-consumer/src/main/java/com/ccr/eurekaconsumer/SimpleService.java
+++ spring-cloud-demo/eureka-consumer/src/main/java/com/ccr/eurekaconsumer/SimpleService.java
@@ -1,10 +1,12 @@
 package com.ccr.eurekaconsumer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ * 在hiService方法上加上@HystrixCommand注解。该注解对该方法创建了熔断器的功能，并指定了fallbackMethod熔断方法，熔断方法直接返回了一个字符串
  * @author ccr12312@163.com at 2019-1-23
  */
 @Service
@@ -13,7 +15,12 @@ public class SimpleService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String getInfo(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
